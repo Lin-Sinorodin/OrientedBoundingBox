d631740cce15f9d5cc38b82c5a18a82d253269ad
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-10-31 09:54:44 +0200

    add xywha conversion

--- code/utils/data.py
+++ code/utils/data.py
@@ -1,6 +1,17 @@
 import os
+import cv2
 import gdown
 import shutil
+import numpy as np
+
+DOTA_V1_5_NAMES = [
+    'plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle',
+    'large-vehicle', 'ship', 'tennis-court', 'basketball-court', 'storage-tank',
+    'soccer-ball-field', 'roundabout', 'harbor', 'swimming-pool', 'helicopter',
+    'container-crane'
+]
+
+DOTA_NAME_TO_INT = {name: idx for idx, name in enumerate(DOTA_V1_5_NAMES)}
 
 
 class DatasetDownloader:
@@ -35,3 +46,37 @@ class DatasetDownloader:
             gdown.download(id=drive_id, output=zip_path)
             shutil.unpack_archive(filename=zip_path, extract_dir='/'.join(zip_path.split('/')[:-1]))
             os.remove(zip_path)
+
+
+class BoundingBox:
+    def __init__(self, path):
+        self.path = path
+        self.xyxyxyxy, self.objects_class = self._load_from_dota()
+
+    def _load_from_dota(self):
+        """Load a bounding box in the DOTA format ((x1, y1), (x2, y2), (x3, y3), (x4, y4))"""
+        with open(self.path) as f:
+            raw_label = f.read().split('\n')[:-1]
+            raw_label = [line.split(' ') for line in raw_label if line[0].isdigit()]
+            raw_label = np.array(raw_label)
+
+            bounding_boxes = np.float32(raw_label[:, :8])
+            objects_class = [int(DOTA_NAME_TO_INT[name]) for name in raw_label[:, 8]]
+            return bounding_boxes, objects_class
+
+    def _xyxyxyxy_to_xywha(self):
+        xywha = []
+        for bounding_box in self.xyxyxyxy:
+            rect = cv2.minAreaRect(bounding_box.reshape((4, 2)))
+            x_center, y_center = rect[0]
+            width, height = rect[1]
+            angle = rect[2]   # [-90, 0)
+
+            xywha.append([x_center, y_center, width, height, angle])
+
+        return np.float32(np.array(xywha))
+
+    @property
+    def xywha(self):
+        return self._xyxyxyxy_to_xywha()
+
