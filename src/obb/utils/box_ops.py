c37fe09ec469b4de8303cdd2f32e4db6ce02507e
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-12-15 00:39:18 +0200

    refactor box_ops

--- src/obb/utils/box_ops.py
+++ src/obb/utils/box_ops.py
@@ -5,11 +5,23 @@ Credit: https://github.com/jw9730/ori-giou
 import torch
 
 
-def convex_hull(points):
+def cross(o: torch.Tensor, a: torch.Tensor, b: torch.Tensor) -> torch.tensor:
+    """
+    2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+
+    :param o: point O for OA and OB vector.
+    :param a: point A for OA vector.
+    :param b: point B for OB vector.
+    :return: positive if OAB makes a counter-clockwise turn, negative for clockwise, zero if the points are collinear.
+    """
+    return (a[..., 0] - o[..., 0]) * (b[..., 1] - o[..., 1]) - (a[..., 1] - o[..., 1]) * (b[..., 0] - o[..., 0])
+
+
+def convex_hull(points: torch.Tensor):
     """
     PyTorch-Compliant, vectorized convex hull implementation using the monotone chain algorithm.
-    Details about the algorithm: https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
-    TODO Add CUDA support
+    Details about the algorithm:
+        https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
 
     :param points: (Tensor[..., N, 2]) Arbitrarily-dimensional tensor, with he last two dimensions
     containing sets of N points.
@@ -17,12 +29,6 @@ def convex_hull(points):
     counterclockwise. Cells after the last index in convex hull are assigned arbitrary values.
     2. (Tensor[...]) Tensor of the number of points belonging to the convex hull of each set.
     """
-
-    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
-    # Returns a positive value, if OAB makes a counter-clockwise turn,
-    # negative for clockwise turn, and zero if the points are collinear.
-    def cross(o, a, b):
-        return (a[..., 0] - o[..., 0]) * (b[..., 1] - o[..., 1]) - (a[..., 1] - o[..., 1]) * (b[..., 0] - o[..., 0])
 
     D = points.shape[0]  # Dimensions excluding cluster size and coordinate number (2)
     # TODO Add support for Arbitrarily-dimensional tensors (current version works only when points.shape[:-2] is 1D).
@@ -73,7 +79,7 @@ def convex_hull(points):
     # Concatenation of the lower and upper hulls gives the convex hull.
     # Last point of each list is omitted because it is repeated at the beginning of the other list.
 
-    hull = torch.zeros(points.shape).fill_(torch.nan)  # [..., N, 2]
+    hull = torch.zeros(points.shape).fill_(float('nan'))  # [..., N, 2]
     sizes = torch.zeros(D).long()  # [...]
 
     for k in range(N):
@@ -96,32 +102,33 @@ def convex_hull(points):
     return hull, sizes
 
 
-def min_area_rect(points):
+def apply_rot(points: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor, inv: bool = False):
+    """Applying passive rotation by angle theta on points, given cos(theta), sin(theta)."""
+    cos = cos.unsqueeze(-1)  # [..., 1]
+    sin = sin.unsqueeze(-1)  # [..., 1]
+
+    if inv:
+        return torch.stack([cos * points[..., 0] - sin * points[..., 1],
+                            sin * points[..., 0] + cos * points[..., 1]], dim=-1)
+    else:
+        return torch.stack([cos * points[..., 0] + sin * points[..., 1],
+                            -sin * points[..., 0] + cos * points[..., 1]], dim=-1)
+
+
+def min_area_rect(points: torch.Tensor) -> torch.Tensor:
     """
     PyTorch-Compliant, vectorized convex hull implementation of the cv2 MinAreaRect function, which finds for a given
-    set of points the bounding rectangle with the minimal area. The algorithm works by computing the convex hull of the
-    point set, finding the minimal rectangles with sides parallel to those of the hull, and choosing the rectangle with
-    the minimal area from them.
-    Implementation inspired by: https://gis.stackexchange.com/questions/22895/finding-minimum-area-rectangle-for-given-points
-    TODO Add CUDA support
+    set of points the bounding rectangle with the minimal area.
+    The algorithm works by computing the convex hull of the point set, finding the minimal rectangles with sides
+    parallel to those of the hull, and choosing the rectangle with the minimal area from them.
+    Implementation inspired by:
+        https://gis.stackexchange.com/questions/22895/finding-minimum-area-rectangle-for-given-points
 
     :param points: (Tensor[..., N, 2]) Arbitrarily-dimensional tensor, with he last two dimensions
     containing sets of N points.
     :return: (Tensor[..., 4, 2]) Tensor containing the minimal rectangle for each point set in
     (x0, y0),...,(x3, y3) format, sorted counterclockwise.
     """
-
-    def apply_rot(points, c, s, inv=False):
-        """Applying passive rotation by angle theta on points, given c = cos(theta), s = sin(theta)."""
-        c = c.unsqueeze(-1)  # [..., 1]
-        s = s.unsqueeze(-1)  # [..., 1]
-
-        if inv:
-            return torch.stack([c * points[..., 0] - s * points[..., 1],
-                                s * points[..., 0] + c * points[..., 1]], dim=-1)
-        else:
-            return torch.stack([c * points[..., 0] + s * points[..., 1],
-                                -s * points[..., 0] + c * points[..., 1]], dim=-1)
 
     D = points.shape[0]  # Dimensions excluding cluster size and coordinate number (2)
     # TODO Add support for Arbitrarily-dimensional tensors (current version works only when points.shape[:-2] is 1D).
@@ -137,21 +144,21 @@ def min_area_rect(points):
     angles = torch.atan2(edges[..., 1], edges[..., 0])  # [..., N]
 
     rect_min = torch.zeros(D, 4, 2)  # [..., 4, 2]
-    area_min = torch.zeros(D).fill_(torch.inf)  # [...]
+    area_min = torch.zeros(D).fill_(float('inf'))  # [...]
 
     for k in range(N):
         # Apply rotation by corresponding edge angles
         theta = angles[..., k]
-        c = torch.cos(theta)  # [...]
-        s = torch.sin(theta)  # [...]
+        cos = torch.cos(theta)  # [...]
+        sin = torch.sin(theta)  # [...]
 
-        hull_rot = apply_rot(hull, c, s, inv=False)  # [..., N, 2]
+        hull_rot = apply_rot(hull, cos, sin, inv=False)  # [..., N, 2]
 
         # Find minimal and maximal x, y coordinates in the rotated frame
-        x_min, _ = torch.min(hull_rot[..., 0].nan_to_num(torch.inf), dim=-1)  # [...]
-        x_max, _ = torch.max(hull_rot[..., 0].nan_to_num(-torch.inf), dim=-1)  # [...]
-        y_min, _ = torch.min(hull_rot[..., 1].nan_to_num(torch.inf), dim=-1)  # [...]
-        y_max, _ = torch.max(hull_rot[..., 1].nan_to_num(-torch.inf), dim=-1)  # [...]
+        x_min, _ = torch.min(hull_rot[..., 0].nan_to_num(float('inf')), dim=-1)  # [...]
+        x_max, _ = torch.max(hull_rot[..., 0].nan_to_num(-float('inf')), dim=-1)  # [...]
+        y_min, _ = torch.min(hull_rot[..., 1].nan_to_num(float('inf')), dim=-1)  # [...]
+        y_max, _ = torch.max(hull_rot[..., 1].nan_to_num(-float('inf')), dim=-1)  # [...]
 
         # Calculate area of resultant rectangle
         area = (x_max - x_min) * (y_max - y_min)
@@ -164,6 +171,6 @@ def min_area_rect(points):
         rect_y = torch.stack([y_min, y_min, y_max, y_max], dim=-1)  # [..., 4]
         rect = torch.stack([rect_x, rect_y], dim=-1)  # [..., 4, 2]
 
-        rect_min[mask] = apply_rot(rect[mask], c[mask], s[mask], inv=True)
+        rect_min[mask] = apply_rot(rect[mask], cos[mask], sin[mask], inv=True)
 
     return rect_min
