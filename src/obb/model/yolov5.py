c80dfe205a54ca5b12b2ed45bc1fc87acaba27a3
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2022-04-16 16:31:44 +0300

    Fix bugs in yolov5 backend

--- src/obb/model/yolov5.py
+++ src/obb/model/yolov5.py
@@ -2,41 +2,47 @@ import os
 import torch
 import torch.nn as nn
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class YOLOv5Features(nn.Module):
     def __init__(self, weights_dir='weights', requires_grad=False):
         super().__init__()
         self.weights_dir = weights_dir
         self.requires_grad = requires_grad
-        # self.weights_path = f'{self.weights_dir}/best.pt'
-        self.weights_path = './yolov5.pt'
+        self.weights_path = self._download_weights()
 
-        self.model = self._load_model()
-        self.model_sequential = list(list(list(self.model.children())[0].children())[0].children())[0]
-        self.prev_layers = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, [-1, 6], -1, -1,
-                            -1, [-1, 4], -1, -1, [-1, 14], -1, -1, [-1, 10], -1, [17, 20, 23]]
+        self.features = self._load_model()
+
+        conv_kwargs = {'kernel_size': 3, 'padding': 1, 'bias': False, 'device': device}
+        self.P3_resample = nn.Conv2d(in_channels=192, out_channels=256, **conv_kwargs)
+        self.P4_resample = nn.Conv2d(in_channels=384, out_channels=256, **conv_kwargs)
+        self.P5_resample = nn.Conv2d(in_channels=768, out_channels=256, **conv_kwargs)
 
     def _download_weights(self):
         """Download pretrained weights from: https://github.com/hukaixuan19970627/YOLOv5_DOTA_OBB"""
         os.makedirs(self.weights_dir, exist_ok=True)
-        weights_path = f'{self.weights_dir}/best.pt'
-        # if 'YOLOv5_DOTAv1.5_OBB.pt' not in os.listdir(self.weights_dir):
-        #     gdown.download(id='171xlq49JEiKJ3L-UEV9tICXltPs92dLk', output=weights_path)
+        weights_path = f'{self.weights_dir}/yolov5.pt'
+        if 'yolov5.pt' not in os.listdir(self.weights_dir):
+            url = 'https://drive.google.com/file/d/1tS0zvLBTuG2VWTNIlKb15qFKxGh43MTu/view?usp=sharing'
+            raise ValueError(f'Please download the weights from {url} to the weights directory.')
         return weights_path
 
     def _load_model(self):
-        model = torch.hub.load('ultralytics/yolov5', 'custom', self.weights_path)
+        model = torch.hub.load('ultralytics/yolov5', 'custom', self.weights_path).to(device)
 
         for k, v in model.named_parameters():
             v.requires_grad = self.requires_grad
 
-        return model
+        return list(list(list(model.children())[0].children())[0].children())[0][:-1]
 
     def forward(self, img):
         """Get features from YOLOv5 Backbone+Neck on the given image"""
+        prev_layers = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, [-1, 6], -1, -1,
+                       -1, [-1, 4], -1, -1, [-1, 14], -1, -1, [-1, 10], -1, [17, 20, 23]]
+
         x, y = img, []
-        for layer_idx, layer in enumerate(self.model_sequential[:-1]):
-            prev_layer = self.prev_layers[layer_idx]
+        for layer_idx, layer in enumerate(self.features):
+            prev_layer = prev_layers[layer_idx]
             if prev_layer != -1:  # if current layer is concat or SPP
                 if isinstance(prev_layer, int):
                     x = y[prev_layer]
@@ -48,13 +54,9 @@ class YOLOv5Features(nn.Module):
 
         P3, P4, P5 = [i for i in y if i is not None][-3:]
 
-        P3_resample = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=3, padding=1, bias=False)
-        P4_resample = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1, bias=False)
-        P5_resample = nn.Conv2d(in_channels=768, out_channels=256, kernel_size=3, padding=1, bias=False)
-
-        P3 = P3_resample(P3)
-        P4 = P4_resample(P4)
-        P5 = P5_resample(P5)
+        P3 = self.P3_resample(P3)
+        P4 = self.P4_resample(P4)
+        P5 = self.P5_resample(P5)
 
         P3.stride = 4
         P4.stride = 8
@@ -63,16 +65,17 @@ class YOLOv5Features(nn.Module):
         return P3, P4, P5
 
 
-# if __name__ == '__main__':
-#     from torch.utils.data import DataLoader
-#     from obb.utils.dataset import Dataset
-#
-#     train_dataset = Dataset(path='../../../assets/DOTA_sample_data/split')
-#     train_data_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
-#
-#     yolov5 = YOLOv5Features()
-#
-#     for img, obb, object_class in train_data_loader:
-#         P3, P4, P5 = yolov5(img)
-#         print(f'{P3.shape = }, {P3.stride = }\n{P4.shape = }, {P4.stride = }\n{P5.shape = }, {P5.stride = }')
-#         break
+if __name__ == '__main__':
+    from torch.utils.data import DataLoader
+    from obb.utils.dataset import Dataset
+
+    train_dataset = Dataset(path='../../../assets/DOTA_sample_data/split')
+    train_data_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+
+    yolov5 = YOLOv5Features()
+
+    for img, obb, object_class in train_data_loader:
+        P3, P4, P5 = yolov5(img)
+        print(f'{P3.shape = }, {P3.stride = }\n{P4.shape = }, {P4.stride = }\n{P5.shape = }, {P5.stride = }')
+        break
+
