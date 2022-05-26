ecdedd15a7ec4ac6ca42739519c7ffabc4a2518b
matanhaller <matan.haller@gmail.com>
2022-05-26 03:27:30 +0300

    Add precision/recall metrics to loss function output

--- src/obb/model/train.py
+++ src/obb/model/train.py
@@ -20,7 +20,7 @@ def elapsed_time(start_time):
 
 
 if __name__ == '__main__':
-    train_dataset = Dataset(path='../../../assets/DOTA/train_split')
+    train_dataset = Dataset(path='../../../assets/DOTA_sample_data/split')
     train_data_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
     model = DetectionModel().to(device)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     learning_rate = 1e-3
     weight_decay = 5e-5
-    epochs = 10
+    epochs = 200
 
     # optimizer = torch.optim.Adam(
     #     params=model.parameters(),
@@ -52,53 +52,37 @@ if __name__ == '__main__':
         model.train()
 
         running_loss = 0.0
-        running_cls_loss = 0.0
-        running_reg_loss_init = 0.0
-        running_reg_loss_refine = 0.0
-        running_prcs_pos = 0.0
-        running_prcs_neg = 0.0
+        running_classification_loss = 0.0
+        running_regression_init_loss = 0.0
+        running_regression_refine_loss = 0.0
 
         cnt = 0
-        cnt_pos = 0
-        log_step = 100
+        log_step = 1
         epoch_time = time.time()
         for img, obb, object_class in tqdm(train_data_loader):
 
             img = img.to(device)
             obb = obb.squeeze(dim=0)
             object_class = object_class.squeeze(dim=0)
-            if len(object_class) == 0:
-                continue
 
             rep_points_init, rep_points_refine, classification = model(img)  # forward pass
 
-            loss, cls_loss, reg_loss_init, reg_loss_refine, prcs_pos, prcs_neg = rep_points_loss.get_loss(
-                rep_points_init,
-                rep_points_refine,
-                classification,
-                obb, object_class)  # calculate loss
+            loss, loss_dict = rep_points_loss.get_loss(rep_points_init, rep_points_refine, classification, obb, object_class)  # calculate loss
             optimizer.zero_grad()  # zero the parameter gradients
             loss.backward()  # backpropagation
             nn.utils.clip_grad_norm_(model.parameters(), max_norm=35, norm_type=2)  # clip gradient
             optimizer.step()  # update parameters
 
             running_loss = (cnt * running_loss + loss.data.item()) / (cnt + 1)
-            running_cls_loss = (cnt * running_cls_loss + cls_loss.data.item()) / (cnt + 1)
-            if len(object_class) > 0:
-                running_prcs_pos = (cnt_pos * running_prcs_pos + prcs_pos.data.item()) / (cnt_pos + 1)
-                running_reg_loss_init = (cnt_pos * running_reg_loss_init + reg_loss_init.data.item()) / (cnt_pos + 1)
-                running_reg_loss_refine = (cnt_pos * running_reg_loss_refine + reg_loss_refine.data.item()) / (cnt_pos + 1)
-                cnt_pos += 1
-            running_prcs_neg = (cnt * running_prcs_neg + prcs_neg.data.item()) / (cnt + 1)
 
             cnt += 1
             if cnt % log_step == 0:
-                print(f'{running_cls_loss:.4f}, '
-                      f'{running_reg_loss_init:.4f}, {running_reg_loss_refine:.4f}, '
-                      f'{running_prcs_pos:.4f}, {running_prcs_neg:.4f}\t\t\t\t'
-                      f'{cls_loss.data.item():.4f}, '
-                      f'{float(reg_loss_init):.4f}, {float(reg_loss_refine):.4f}, '
-                      f'{float(prcs_pos):.4f}, {float(prcs_neg):.4f}, {len(object_class)}')
+                print()
+                print(float(loss_dict['classification']))
+                print(float(loss_dict['regression_init']))
+                print(float(loss_dict['regression_refine']))
+                print([f'{prc_cls:.4f}' for prc_cls in loss_dict['precision']])
+                print([f'{rcl_cls:.4f}' for rcl_cls in loss_dict['recall']])
 
         # calculate loss
         train_loss_dict[epoch] = running_loss
